ea3201205a5d90c80bc00aec5e0a2803a7ca0a17
Clayton Pereira <cpereiramt@gmail.com>
2020-04-29 13:45:22 -0400

    Salvando as alteracoes no requisito SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function SearchBar(props) {
   const {
-    searchText, onSearchTextChange, bookmarkedOnly,
-    onBookmarkedChange, selectedGenre, onSelectedGenreChange,
+    searchText, onSearchText, bookmarkedOnly,
+    onBookmarked, selectedGenre, onSelectedGenre,
   } = props;
 
   const handleForm = (event) => {
@@ -12,12 +12,11 @@ function SearchBar(props) {
 
   return (<form onChange={handleForm}>
       <label htmlFor="search"> Inclui o texto</label>
-      <input type="text" id="search" value={searchText } onChange={onSearchTextChange }/>
+      <input type="text" id="search" value={searchText } onChange={onSearchText }/>
       <label htmlFor="seleectFavorito">Mostrar somente favoritos</label>
-      <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}
-        name="seleectFavorito" />
+      <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarked} name="seleectFavorito" />
      <label htmlFor="selectedGenero">Filtrar por gênero</label>
-     <select value={selectedGenre} onChange={onSelectedGenreChange} name="selectedGenero">
+     <select value={selectedGenre} onChange={onSelectedGenre} name="selectedGenero">
         <option value="">Todos</option>
         <option value="action">Ação</option>
         <option value="comedy">Comédia</option>
