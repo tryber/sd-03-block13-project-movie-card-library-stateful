5a48fb834b7091265b3dc1533113e62fcc454178
Clayton Pereira <cpereiramt@gmail.com>
2020-04-29 16:00:20 -0400

    Salvando as alteracoes no requisito AddMove

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,55 +5,50 @@ import MovieList from './MovieList';
 import AddMove from './AddMovie';
 
 class MovieLibrary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchText: '',
-            bookmarkedOnly: false,
-            selectedGenre: '',
-            movies: props.movies,
-        };
-        this.onSearchTextChange = this.onSearchTextChange.bind(this);
-        this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-        this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-        this.filterMovies = this.filterMovies.bind(this);
-        this.state.movies.map(element => console.log(element.title))
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
+    this.state.movies.map((element) => console.log(element.title));
+  }
 
-    onSearchTextChange(event) {
-        this.setState({ searchText: event.target.value });
-    }
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
 
-    onBookmarkedChange(event) {
-        this.setState({ bookmarkedOnly: event.target.checked });
-    }
+  onBookmarkedChange(event) {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
 
-    onSelectedGenreChange(event) {
-        this.setState({ selectedGenre: event.target.value });
-    }
+  onSelectedGenreChange(event) {
+    this.setState({ selectedGenre: event.target.value });
+  }
 
-    filterMovies() {
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.onBookmarkedChange}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+          selectedGenre={this.state.selectedGenre}
+        />
 
-
-    }
-
-    render() {
-        return (
-            <div>
-                <SearchBar
-                    searchText={this.state.searchText}
-                    onSearchTextChange={this.onSearchTextChange}
-                    bookmarkedOnly={this.state.bookmarkedOnly}
-                    onBookmarkedChange={this.onBookmarkedChange}
-                    onSelectedGenreChange={this.onSelectedGenreChange}
-                    selectedGenre={this.state.selectedGenre}
-                />
-
-                <MovieList movies={this.state.movies} />
-                <AddMove />
-            </div>
-        );
-    }
+        <MovieList movies={this.state.movies} />
+        <AddMove />
+      </div>
+    );
+  }
 }
 
 export default MovieLibrary;
