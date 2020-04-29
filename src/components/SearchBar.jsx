e6d6dd1d5d2f291bdf29140dca4add565aac6618
DiegoRafaelAF <draf0314@gmail.com>
2020-04-29 20:52:26 -0300

    Code Climate 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,33 +2,21 @@ import React from 'react';
 
 function SearchBar(props) {
   const {
-    searchText,
-    onSearchTextChange,
-    bookmarkedOnly,
-    onBookmarkedChange,
-    selectedGenre,
-    onSelectedGenreChange,
+    searchText, onSearchTextChange, bookmarkedOnly,
+    onBookmarkedChange, selectedGenre, onSelectedGenreChange,
   } = props;
   return (
     <form className="form">
-      <label htmlFor="IncluiOtexto"><span>Inclui o texto:&nbsp;</span>
-        <input
-          id="IncluiOtexto"
-          onChange={onSearchTextChange}
-          type="text"
-          value={searchText}
-        />
+      <label htmlFor="IncluiOtexto">
+        <span>Inclui o texto:&nbsp;</span>
+        <input id="IncluiOtexto" onChange={onSearchTextChange} type="text" value={searchText} />
       </label>
       <label htmlFor="MostrarSomenteFavoritos">
-        <input
-          checked={bookmarkedOnly}
-          id="MostrarSomenteFavoritos"
-          name="bookmarkedOnly"
-          onChange={onBookmarkedChange}
-          type="checkbox"
-        /><span>&nbsp;Mostrar somente favoritos</span>
+        <input checked={bookmarkedOnly} id="MostrarSomenteFavoritos" name="bookmarkedOnly" onChange={onBookmarkedChange} type="checkbox" />
+        <span>&nbsp;Mostrar somente favoritos</span>
       </label>
-      <label htmlFor="FiltrarPorGenero"><span>Filtrar por gênero&nbsp;</span>
+      <label htmlFor="FiltrarPorGenero">
+        <span>Filtrar por gênero&nbsp;</span>
         <select id="FiltrarPorGenero" value={selectedGenre} onChange={onSelectedGenreChange}>
           <option value="">Todos</option>
           <option value="action">Ação</option>
