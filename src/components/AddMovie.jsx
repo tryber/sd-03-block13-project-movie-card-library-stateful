353d65fc13cbcbbf4e65b1ce6a0bb664472b1096
Clayton Pereira <cpereiramt@gmail.com>
2020-04-29 15:22:52 -0400

    Salvando as alteracoes no requisito AddMove

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,10 +13,10 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
     this.alteraTitulo = this.alteraTitulo.bind(this);
-    this.alteraSubtitulo = this.alteraSubtitulo.bind(this);
+    this.alteraSubtitulo = this.alteraSub.bind(this);
     this.alteraImagem = this.alteraImagem.bind(this);
-    this.alteraStory = this.alteraStory.bind(this);
-    this.alteraRating = this.alteraRating.bind(this);
+    this.alteraStory = this.altsto.bind(this);
+    this.alteraRating = this.alteraRat.bind(this);
     this.resetaState = this.resetaState.bind(this);
   }
 
@@ -24,7 +24,7 @@ class AddMovie extends React.Component {
     this.setState({ title: event.target.value });
   }
 
-  alteraSubtitulo(event) {
+  alteraSub(event) {
     this.setState({ subtitle: event.target.value });
   }
 
@@ -32,11 +32,11 @@ class AddMovie extends React.Component {
     this.setState({ imagePath: event.target.value });
   }
 
-  alteraStory(event) {
+  altsto(event) {
     this.setState({ storyline: event.target.value });
   }
 
-  alteraRating(event) {
+  alteraRat(event) {
     this.setState({ rating: Number(event.target.value) });
   }
 
@@ -61,28 +61,28 @@ class AddMovie extends React.Component {
 
   render() {
     return (
-     <form>
+      <form>
        <label htmlFor="titulo">Título</label>
        <input type="text" value={this.state.title} onChange={this.alteraTitulo} id="titulo" />
        <label htmlFor="subtitulo">Subtítulo</label>
-       <input type="text" value={this.state.subtitle} onChange={this.alteraSubtitulo} id="subtitulo" />
+       <input type="text" value={this.state.subtitle} onChange={this.alteraSub} id="subtitulo" />
        <label htmlFor="imagem">Imagem</label>
        <input type="text" value={this.state.imagePath} onChange={this.alteraImagem} name="imagem" />
-       <label htmlFor="sinopse">Sinopse</label>
-       <textarea type="text" value={this.state.storyline } onChange={this.alteraStory } name="sinopse"/>
+       <label htmlFor="sinops">Sinopse</label>
+       <textarea type="text" value={this.state.storyline} onChange={this.altsto} name="sinops"/>
        <label htmlFor="avaliacao">Avaliação</label>
-       <input type="number" value={this.state.rating} onChange={this.alteraRating} name="avaliacao" />
+       <input type="number" value={this.state.rating} onChange={this.alteraRat} name="avaliacao" />
        <label htmlFor="genero">
        Gênero
        </label>
        <select value={this.state.genre} onChange={this.alteraSelectGenre} name="genero">
-       <option value="action">Ação</option>
-       <option value="comedy">Comédia</option>
-       <option value="thriller">Suspense</option>
+         <option value="action">Ação</option>
+         <option value="comedy">Comédia</option>
+         <option value="thriller">Suspense</option>
        </select>
        <button onClick={this.resetaState}>Adicionar filme</button>
-     </form>
-   );
+      </form>
+    );
   }
 }
 
