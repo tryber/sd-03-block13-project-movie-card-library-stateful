425f77845a72aa3bdb1bf7ebbf82567277ed57b7
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-29 17:33:24 -0300

    firsts changes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const input = e.target;
+    this.setState({ name: input.value });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <legend>New Movie</legend>
+            <input 
+              type="text" 
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const input = e.target;
+    this.setState({ name: input.value });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <legend>Search Movie</legend>
+            <input 
+              type="text" 
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
