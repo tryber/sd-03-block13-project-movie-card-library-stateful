8d2e7936000eafcace172d770394cb3e080756f3
Hfreitas <hfreitas86@gmail.com>
2020-04-29 17:36:50 -0300

    alterada implementação do componente SearchBar, o state será gerenciado por outro componente e o componente foi refatorado como função

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,66 +1,52 @@
 // implement SearchBar component here
 import React from 'react';
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { searchText: '', bookmarkedOnly: true, selectedGenre: '' };
-    this.onSearchTextChange = this.onSearchTextChange.bind(this);
-    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-  }
-
-  onSearchTextChange() {
-    console.log('retornando', this);
-  }
-
-  onBookmarkedChange() {
-    console.log('retornando', this);
-  }
-
-  onSelectedGenreChangeChange() {
-    console.log('retornando', this);
-  }
-
-  render() {
-    return (
-      <form>
-        <label htmlFor="search-text">
-          Inclui o texto:
-          <input
-            type="text"
-            value={this.state.searchText}
-            id="search-text"
-            onChange={this.onSearchTextChange}
-          />
-        </label>
-        <label htmlFor="search-checkbox">
-          Mostrar somente favoritos
-          <input
-            type="checkbox"
-            name="favorite-books"
-            id="search-checkbox"
-            onChange={this.onSelectedGenreChangeChange}
-            checked={this.state.bookmarkedOnly}
-          />
-        </label>
-        <label htmlFor="genre-select">
-          <select
-            name="genre-select"
-            id="genre-select"
-            onChange={this.onBookmarkedChange}
-            value={this.state.selectedGenre}
-          >
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
-      </form>
-    );
-  }
+function SearchBar(props) {
+  const {
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
+  } = props;
+  return (
+    <form>
+      <label htmlFor="search-text">
+        Inclui o texto:
+        <input
+          type="text"
+          value={searchText}
+          id="search-text"
+          onChange={onSearchTextChange}
+        />
+      </label>
+      <label htmlFor="search-checkbox">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          name="favorite-books"
+          id="search-checkbox"
+          onChange={onBookmarkedChange}
+          checked={bookmarkedOnly}
+        />
+      </label>
+      <label htmlFor="genre-select">
+      Filtrar por gênero
+        <select
+          name="genre-select"
+          id="genre-select"
+          onChange={onSelectedGenreChange}
+          value={selectedGenre}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    </form>
+  );
 }
 
 export default SearchBar;
