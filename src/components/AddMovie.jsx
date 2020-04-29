9b246c4521845a2be368785053485d36e5af40fe
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-29 19:28:56 -0300

    code climate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -36,9 +36,9 @@ class AddMovie extends React.Component {
   }
 
   somelabels() {
-    const { title, subtitle, imagePath } = this.state;
+    const { title, subtitle, imagePath, storyline, rating } = this.state;
     return (
-      <>
+      <div>
         <label htmlFor="input">
           Título
           <input type="text" value={title} name="title" onChange={this.handleChange} />
@@ -51,32 +51,32 @@ class AddMovie extends React.Component {
           Imagem
           <input type="text" value={imagePath} name="imagePath" onChange={this.handleChange} />
         </label>
-      </>
+        <label htmlFor="input">
+          Sinopse
+          <textarea value={storyline} name="storyline" onChange={this.handleChange} />
+        </label>
+        <div>
+          <label htmlFor="rating">
+            Avaliação
+            <input
+              type="number"
+              name="rating"
+              value={rating}
+              placeholder="Avaliação"
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
+      </div>
     );
   }
 
   render() {
-    const { storyline, rating, genre } = this.state;
+    const { genre } = this.state;
     return (
       <div>
         <form>
           {this.somelabels()}
-          <label htmlFor="input">
-          Sinopse
-            <textarea value={storyline} name="storyline" onChange={this.handleChange} />
-          </label>
-          <div>
-            <label htmlFor="rating">
-              Avaliação
-              <input
-                type="number"
-                name="rating"
-                value={rating}
-                placeholder="Avaliação"
-                onChange={this.handleChange}
-              />
-            </label>
-          </div>
           <div>
             <label htmlFor="rating">
               Avaliação
