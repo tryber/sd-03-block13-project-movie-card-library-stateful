9a7bf73a2a63c73bb0b170915368a310bff6bb7b
henrique <hbaetaleite@gmail.com>
2020-04-29 18:35:10 -0300

    testando primeiros componentes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,9 +14,30 @@ export default class AddMovie extends Component {
       genre: 'action',
     }
   }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+
+
   render() {
     return (
       <div>
+        <form className="movie-card" action="">
+
+          <label htmlFor="title">Título</label>
+          <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+          <br/>
+          <label htmlFor="subtitle">Subtítulo</label>
+          <input type="text" name="subtitle" value={this.state.subtitle} onChange={this.handleChange} />
+          <br/>
+          <label htmlFor="imagem">Imagem</label>
+          <input type="text" name="imagePath" value={this.state.imagePath} onChange={this.handleChange} />
+          <br/>
+
+
+        </form>
 
       </div>
     );
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,18 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie onClick={this.props} />
+        <h1>teste</h1>
+      </div>
+    );
+  }
+}
