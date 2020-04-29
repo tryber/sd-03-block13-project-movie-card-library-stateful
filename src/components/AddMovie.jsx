6bb7157294bc58197fd67de5afb74409b6b1e8b9
Gustaff <gustavohmfigueiredo@gmail.com>
2020-04-29 11:06:38 -0300

    SearchBar passando nos testes, mas nao chamado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React from 'react';
+
+export default class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state({
+      gender: 'action',
+      imagePath: '',
+      rating: 0,
+      storyline: '',
+      subtitle: '',
+      title: '',
+    });
+  }
+
+  render() {
+    const { gender, imagePath, rating, storyline, subtitle, title } = this.state;
+    return (
+      <form>
+        <label>Título
+          <input 
+            type={'text'}
+          />
+        </label>
+      </form>
+    );
+  }
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
-// implement SearchBar component here
+import React from 'react';
+
+function SearchBar(props) {
+  return (
+    <form>
+      <label>Inclui o texto:
+        <input
+          type={'text'}
+          value={props.searchText}
+          onChange={props.onSearchTextChange}
+        />
+      </label>
+      <label>Mostrar somente favoritos
+        <input
+          type={'checkbox'}
+          checked={props.bookmarkedOnly}
+          onChange={props.onBookmarkedChange}
+      />
+      </label>
+      <label>Filtrar por gênero
+        <select
+          value={props.selectedGenre}
+          onChange={props.onSelectedGenreChange}
+        >
+          <option value={''}>Todos</option>
+          <option value={'action'}>Ação</option>
+          <option value={'comedy'}>Comédia</option>
+          <option value={'thriller'}>Suspense</option>
+        </select>
+      </label>
+    </form>
+  )
+}
+
+export default SearchBar;
