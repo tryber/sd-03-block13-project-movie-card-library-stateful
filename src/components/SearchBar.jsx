13379a41c1055e01813382ab9a7afb5a38178e00
Clayton Pereira <cpereiramt@gmail.com>
2020-04-29 16:49:36 -0400

    Ajustando o componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,8 +9,8 @@ function SearchBar(props) {
   return (<form>
     <label htmlFor="search"> Inclui o texto</label>
     <input type="text" id="search" value={searchText} onChange={onSearchTextChange} />
-    <label htmlFor="seleectFavorito">Mostrar somente favoritos</label>
-    <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} name="seleectFavorito" />
+    <label htmlFor="selectF">Mostrar somente favoritos</label>
+    <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} name="selectF" />
     <label htmlFor="selectedGenero">Filtrar por gênero</label>
     <select value={selectedGenre} onChange={onSelectedGenreChange} name="selectedGenero">
       <option value="">Todos</option>
