95e0869604cb3a4f65d925d01c5cef6417c7e62a
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-04-29 17:35:43 -0300

    Iniciando o Projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,49 @@
-// implement AddMovie component here
+import React from './react'
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: 'action'
+    }
+  }
+
+  onChangeTitle = (event) => {
+    this.setState({ title: event.target.value });
+  }
+
+  onChangeSubtitle = (event) => {
+    this.setState({subtitle: event.target.value});
+  }
+
+  onChangeImagePath = (event) => {
+    this.setState({imagePath: event.target.value})
+  }
+
+  render() {
+    const onClick = this.props.onClick;
+    return (
+      <form>
+        <label>
+          Título
+          <input type="text" value={this.state.title} onChange={this.onChangeTitle}></input>
+        </label>
+        <label>
+          Subtítulo
+          <input type="text" value={this.state.subtitle} onChange={this.onChangeSubtitle}></input>
+        </label>
+        <label>
+          Imagem
+          <input type="text" value={this.state.imagePath} onChange={this.onChangeImagePath}></input>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,37 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const searchText = this.props.searchText;
+    const onSearchTextChange = this.props.onSearchTextChange;
+    const bookmarkedOnly = this.props.bookmarkedOnly;
+    const onBookmarkedChange = this.props.onBookmarkedChange;
+    const selectedGenre = this.props.selectedGenre;
+    const onSelectedGenreChange = this.props.onSelectedGenreChange;
+    return (
+      <div>
+        <form>
+          <label>
+            Inclui o texto
+            <input type="text" value={searchText} onChange={onSearchTextChange}></input>
+          </label>
+          <label>
+            Mostrar somente favoritos
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}></input>
+          </label>
+          <label>
+            Filtrar por gênero
+            <select value={selectedGenre} onChange={onSelectedGenreChange}>
+              <option value={""}>Todos</option>
+              <option value={"action"}>Ação</option>
+              <option value={"comedy"}>Comédia</option>
+              <option value={"thriller"}>Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
