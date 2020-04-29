41205911ea250fa94bb45f77921de6381cc90270
Clayton Pereira <cpereiramt@gmail.com>
2020-04-29 15:48:28 -0400

    Salvando as alteracoes no requisito AddMove

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,59 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMove from './AddMovie';
+
+class MovieLibrary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: '',
+            bookmarkedOnly: false,
+            selectedGenre: '',
+            movies: props.movies,
+        };
+        this.onSearchTextChange = this.onSearchTextChange.bind(this);
+        this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+        this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+        this.filterMovies = this.filterMovies.bind(this);
+        this.state.movies.map(element => console.log(element.title))
+    }
+
+    onSearchTextChange(event) {
+        this.setState({ searchText: event.target.value });
+    }
+
+    onBookmarkedChange(event) {
+        this.setState({ bookmarkedOnly: event.target.checked });
+    }
+
+    onSelectedGenreChange(event) {
+        this.setState({ selectedGenre: event.target.value });
+    }
+
+    filterMovies = () => {
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar
+                    searchText={this.state.searchText}
+                    onSearchTextChange={this.onSearchTextChange}
+                    bookmarkedOnly={this.state.bookmarkedOnly}
+                    onBookmarkedChange={this.onBookmarkedChange}
+                    onSelectedGenreChange={this.onSelectedGenreChange}
+                    selectedGenre={this.state.selectedGenre}
+                />
+
+                <MovieList movies={this.state.movies} />
+                <AddMove />
+            </div>
+        );
+    }
+}
+
+export default MovieLibrary;
