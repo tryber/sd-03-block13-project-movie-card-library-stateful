3f6c9a6f3b23a408c422b9732fcd0b469aa5aa27
henrique <hbaetaleite@gmail.com>
2020-04-29 18:12:22 -0300

    iniciando projeto. Vamos com tudo!

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,38 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    
+  }
+
+
+  render() {
+    return (
+      <form action="">
+        <h3>Search movie</h3>
+        <label htmlFor="Inclui o texto">Inclui o texto: </label>
+        <input name="Inclui o texto" type="text" onChange value={this.props.searchText} />
+
+        <br /><br />
+
+        <label htmlFor="check">Mostrar somente favoritos</label>
+        <input checked="bookmarkedOnly" onChange name="check" type="checkBox" />
+
+        <br /><br />
+
+        <label htmlFor="">Filtrar por gênero</label>
+        <select value="selectedGenre" name="gender" id="gender" onChange>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+
+
+      </form>
+    )
+  }
+}
