810f81832d0f6e7c6e671ea43787a0cb3616a0dc
Cardoso <lsc.cardosoo@gmail.com>
2020-04-30 15:07:57 -0300

    Adicionar inputs no formulário do compotente SearchBar utilizando as props do mesmo como parametro de valores

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,6 +10,23 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange, 
     } = this.props;
+    return(
+      <form>
+        <label htmlFor='searchText'>Inclui o texto:</label>
+        <input type='text' id='searchText' value={searchText} onChange={onSearchTextChange} />
+
+        <label htmlFor='favorits'>Mostrar somente favoritos</label>
+        <input type='checkbox' id='favorits' checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+
+        <label htmlFor='filterGenre'>Filtrar por gênero</label>
+        <select id='filterGenre' value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value='' >Todos</option>
+          <option value='action' >Ação</option>
+          <option value='comedy' >Comédia</option>
+          <option value='thriller' >Suspense</option>
+        </select>
+      </form>
+    );
   }
 }
 
