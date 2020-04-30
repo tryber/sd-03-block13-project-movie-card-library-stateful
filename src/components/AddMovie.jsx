2cc0a1acec5259faeff0b9bf07df119ffdff2b96
Wenderson Belko <contato@wendersonbelko.com>
2020-04-29 23:47:44 -0300

    espero passar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,6 +4,16 @@ import React, { Component } from 'react';
 
 export default class components extends Component {
   render() {
-    return <div />;
+    return (
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <textarea name="" id="" cols="30" rows="10" />
+        <input type="number" name="" id="" />
+        <select name="" id="" />
+        <button />
+      </form>
+    );
   }
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,18 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
+// import { Container } from './styles';
+
+export default class components extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList />
+        <AddMovie />
+      </div>
+    );
+  }
+}
