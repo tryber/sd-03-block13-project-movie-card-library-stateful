b244870a60f5494d070381547cf4b4401b4b9103
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-30 11:09:01 -0300

    searchbar complete

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,9 +7,52 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { movies: props.movies };
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
 
     this.addMovie = this.addMovie.bind(this);
+    this.handleOnBookmarkedChange = this.handleOnBookmarkedChange.bind(this);
+    this.filterMovie = this.filterMovie.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+  }
+
+  handleChangeInput(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleOnBookmarkedChange() {
+    const { bookmarkedOnly } = this.state;
+    this.setState({ bookmarkedOnly: !bookmarkedOnly });
+  }
+
+  checkBookmarked(movie) {
+    const { bookmarkedOnly } = this.state;
+    if (!bookmarkedOnly || (bookmarkedOnly && movie.bookmarked === true)) {
+      return movie;
+    }
+    return false;
+  }
+
+  checkGenre(movie) {
+    const { selectedGenre } = this.state;
+    if ((selectedGenre && movie.genre === selectedGenre) || !selectedGenre) return movie;
+    return false;
+  }
+
+  filterMovie(movies) {
+    const { searchText } = this.state;
+    return movies.filter(
+      (movie) => (movie.title.toLowerCase().includes(searchText.toLowerCase())
+      || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+      || movie.storyline.toLowerCase().includes(searchText.toLowerCase()))
+      && this.checkBookmarked(movie)
+      && this.checkGenre(movie),
+    );
   }
 
   addMovie(movie) {
@@ -17,12 +60,18 @@ class MovieLibrary extends React.Component {
   }
 
   render() {
-    const { movies } = this.state;
+    const { searchText, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={movies} />
+        <SearchBar
+          searchText={searchText}
+          onSearchTextChange={this.handleChangeInput}
+          onBookmarkedChange={this.handleOnBookmarkedChange}
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={this.handleChangeInput}
+        />
+        <MovieList movies={this.filterMovie(movies)} />
         <AddMovie onClick={this.addMovie} />
       </div>
     );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Input from './Input';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -6,6 +7,7 @@ class SearchBar extends React.Component {
     this.state = { name: '' };
 
     this.handleChange = this.handleChange.bind(this);
+    this.firstInputs = this.firstInputs.bind(this);
   }
 
   handleChange(e) {
@@ -14,18 +16,49 @@ class SearchBar extends React.Component {
     console.log(this.state);
   }
 
+  firstInputs() {
+    const { searchText, onSearchTextChange, onBookmarkedChange } = this.props;
+
+    return (
+      <div>
+        <Input
+          name="searchText"
+          text="Inclui o texto:"
+          value={searchText}
+          handler={onSearchTextChange}
+        />
+        <Input
+          name="bookmarkedOnly"
+          input="checkbox"
+          text="Mostrar somente favoritos"
+          handler={onBookmarkedChange}
+        />
+      </div>
+    );
+  }
+
   render() {
-    const { name } = this.state;
+    const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <div>
         <form>
           <fieldset>
             <legend>Search Movie</legend>
-            <input
-              type="text"
-              value={name}
-              onChange={this.handleChange}
-            />
+            {this.firstInputs()}
+            <label htmlFor="selectedGenre">
+            Filtrar por gênero
+              <select
+                name="selectedGenre"
+                id="selectedGenre"
+                value={selectedGenre}
+                onChange={onSelectedGenreChange}
+              >
+                <option value="">Todos</option>
+                <option value="action">Ação</option>
+                <option value="comedy">Comédia</option>
+                <option value="thriller">Suspense</option>
+              </select>
+            </label>
           </fieldset>
         </form>
       </div>
