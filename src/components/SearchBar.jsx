b6ae23743584e60519578887e5e7251260b0d0bc
DiegoRafaelAF <draf0314@gmail.com>
2020-04-30 14:42:55 -0300

    Code Climate 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,17 +7,17 @@ function SearchBar(props) {
   } = props;
   return (
     <form className="form">
-      <label htmlFor="IncluiOtexto">
+      <label htmlFor="texto">
         <span>Inclui o texto:&nbsp;</span>
-        <input id="IncluiOtexto" onChange={onSearchTextChange} type="text" value={searchText} />
+        <input id="texto" onChange={onSearchTextChange} type="text" value={searchText} />
       </label>
-      <label htmlFor="MostrarSomenteFavoritos">
-        <input checked={bookmarkedOnly} id="MostrarSomenteFavoritos" onChange={onBookmarkedChange} type="checkbox" />
+      <label htmlFor="Favoritos">
+        <input checked={bookmarkedOnly} id="Favoritos" onChange={onBookmarkedChange} type="checkbox" />
         <span>&nbsp;Mostrar somente favoritos</span>
       </label>
-      <label htmlFor="FiltrarPorGenero">
+      <label htmlFor="Genero">
         <span>Filtrar por gênero&nbsp;</span>
-        <select id="FiltrarPorGenero" value={selectedGenre} onChange={onSelectedGenreChange}>
+        <select id="Genero" value={selectedGenre} onChange={onSelectedGenreChange}>
           <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
