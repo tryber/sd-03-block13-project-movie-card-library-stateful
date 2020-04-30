2e3cb18022f161c3f5b0a8212b51ea19d7af52ec
William Igor <williamigorsilva@gmail.com>
2020-04-30 15:18:21 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-  // const { searchText, onSearchTextChange, bookmarkedOnly,
-  // onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
+    } = this.props;
     return (
       <form>
-        <label htmlFor="a">Inclui o texto:</label>
-        <input type="text" name="a" value="" onChange="" />
-
-        <label htmlFor="checkbox">Mostrar Somente os Favoritos</label>
-        <input type="checkbox" name="checkbox" checked="" value="" onChange="" />
-
-        <label>Filtar por gênero</label>
-        <select value="" onChange="">
-          <option value="">Todos</option>
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
+        <label htmlFor="searchText">
+          Inclui o texto:
+          <input type="text" name="searchText" value={searchText} onChange={onSearchTextChange} />
+        </label>
+        <label htmlFor="checkbox">
+          Mostrar somente favoritos
+          <input type="checkbox" name="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </label>
+        <label htmlFor="genre">
+          Filtrar por gênero
+          <select value={selectedGenre} name="genre" onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
 }
-
 
 export default SearchBar;
