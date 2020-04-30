a3c9a008f27e9b110b5b1d60faa8a8cd8b0fdf9c
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-30 20:35:06 -0300

    Codeclimate check

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import MovieList from './components/MovieList'
-import MovieData from './data'
+import MovieList from './components/MovieList';
+import MovieData from './data';
 
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      <MovieList movies={MovieData}/>
+      <MovieList movies={MovieData} />
     </div>
   );
 }
