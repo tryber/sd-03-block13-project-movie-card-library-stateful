4b314061e08a99aad54ad529007068c406170680
rogerfaustino <roger_faustino@hotmail.com>
2020-04-30 19:06:38 -0300

    Codeclimate issue

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,25 +25,25 @@ export default class SearchBar extends React.Component {
       selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
-        <form>
-          <label htmlFor="input">
-            Inclui o texto:
-            <input type="text" value={searchText} onChange={onSearchTextChange} />
-          </label>
-          <label htmlFor="input">
-            Mostrar somente favoritos
-            <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-          </label>
-          <label htmlFor="input">
-            Filtrar por gênero
-            <select value={selectedGenre} onChange={onSelectedGenreChange}>
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thrilles">Suspense</option>
-            </select>
-          </label>
-        </form>
+      <form>
+        <label htmlFor="input">
+          Inclui o texto:
+          <input type="text" value={searchText} onChange={onSearchTextChange} />
+        </label>
+        <label htmlFor="input">
+          Mostrar somente favoritos
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </label>
+        <label htmlFor="input">
+          Filtrar por gênero
+          <select value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thrilles">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
