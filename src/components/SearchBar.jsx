516cd51d4129bf45827b406b3220d5276e8d06d9
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-30 16:04:05 -0300

    Input texto na search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,8 +10,15 @@ class SearchBar extends React.Component {
     onSelectedGenreChange
     } = this.props;
     */
+   const {
+    searchText, onSearchTextChange
+   } = this.props;
     return (
-      <form />
+      <form>
+        <label id="searchText"><span>Inclui o texto: </span>
+          <input type="text" name="searchBarText" value={searchText} id="searchText" onChange = {onSearchTextChange} />
+        </label>
+      </form>
     );
   }
 }
