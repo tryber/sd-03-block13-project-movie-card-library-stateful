fa34808abfc8c9b437983ea24ce142e4ec1ac11a
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-30 20:33:19 -0300

    Project start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import MovieList from './components/MovieList'
+import MovieData from './data'
 
 import Header from './components/Header';
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={MovieData}/>
     </div>
   );
 }
