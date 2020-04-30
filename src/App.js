23451c3940de73f04b8c8a717dd5c89cabe16782
Rhian Castro <rhi.castro@gmail.com>
2020-04-30 15:04:43 -0300

    vish

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import MoviesList from './data';
+import SearchBar from './components/SearchBar';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header />
+      <div>
+        <SearchBar />
+      </div>
+      <MovieList movies={MoviesList} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,30 @@
-// implement SearchBar component here
+import React from 'react';
+
+function SearchBar(props) {
+  const {
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
+  } = props;
+
+  return (
+    <form>
+      <label htmlFor="search"> Inclui o texto</label>
+      <input type="text" id="search" value={searchText} onChange={onSearchTextChange} />
+      <label htmlFor="favorites">Mostrar somente favoritos</label>
+      <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} name="selectF" />
+      <label htmlFor="selectedGenere">Filtrar por gênero</label>
+      <select value={selectedGenre} onChange={onSelectedGenreChange} name="selectedGenere">
+        <option value="">Todos</option>
+        <option value="action">Ação</option>
+        <option value="comedy">Comédia</option>
+        <option value="thriller">Suspense</option>
+      </select>
+    </form>
+  );
+}
+
+export default SearchBar;
