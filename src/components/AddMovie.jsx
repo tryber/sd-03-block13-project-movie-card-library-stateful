10b30842ff292accee2985899dc69823dcefdca0
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-30 18:15:53 -0300

    Inicializando o AddMovie e atualizando o titulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,10 +2,30 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState((state) => ({
+      [name]: value
+    }));
+  };
+
   render() {
     return (
       <form>
-        <p>AddMovie</p>
+        <label htmlFor="title"><span>Título</span></label>
+        <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/>
       </form>
     );
   }
