da1b4ee55c31b7afe0cba66edec81c83bcb58fd9
Hfreitas <hfreitas86@gmail.com>
2020-04-30 13:50:58 -0300

    implementação preliminar do componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,38 @@
 // implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import movies from '../data';
+
+export class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
+          onSearchTextChange={() => console.log(this)}
+          onBookmarkedChange={() => console.log(this)}
+          onSelectedGenreChange={() => console.log(this)}
+        />
+        <MovieList movies={this.state.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
