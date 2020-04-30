0af4b477c04d98321b9984cb678b484284f8b770
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-30 17:31:43 -0300

    codeclimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,41 +3,31 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    /*
-    const {
-    searchText, onSearchTextChange,
-    bookmarkedOnly, onBookmarkedChange, selectedGenre,
-    onSelectedGenreChange
-    } = this.props;
-    */
     const {
       searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
       <form>
         <div>
-          <label htmlFor="searchText"><span>Inclui o texto:</span>
-            <input
-              type="text" name="searchBarText" value={searchText} id="searchText"
-              onChange={onSearchTextChange}
-            />
-          </label>
-          <label htmlFor="checkbox"><span>Mostrar somente favoritos</span>
-            <input
-              type="checkbox" name="searchBarCheckbox" id="checkbox"
-              checked={bookmarkedOnly} onChange={onBookmarkedChange}
-            />
-          </label>
+          <label htmlFor="searchText" /><span>Inclui o texto:</span>
+          <input
+            type="text" name="searchBarText" value={searchText} id="searchText"
+            onChange={onSearchTextChange}
+          />
+          <label htmlFor="checkbox" /><span>Mostrar somente favoritos</span>
+          <input
+            type="checkbox" name="searchBarCheckbox" id="checkbox"
+            checked={bookmarkedOnly} onChange={onBookmarkedChange}
+          />
         </div>
-        <label htlFor="gender"><span>Filtrar por gênero</span>
-          <select value={selectedGenre} id="gender" onChange={onSelectedGenreChange}>
-            <option key="" value="">Todos</option>
-            <option key="action" value="action">Ação</option>
-            <option key="comedy" value="comedy">Comédia</option>
-            <option key="thriller" value="thriller">Suspense</option>
-          </select>
-        </label>
+        <label htmlFor="gender" /><span>Filtrar por gênero</span>
+        <select value={selectedGenre} id="gender" onChange={onSelectedGenreChange}>
+          <option key="" value="">Todos</option>
+          <option key="action" value="action">Ação</option>
+          <option key="comedy" value="comedy">Comédia</option>
+          <option key="thriller" value="thriller">Suspense</option>
+        </select>
       </form>
     );
   }
