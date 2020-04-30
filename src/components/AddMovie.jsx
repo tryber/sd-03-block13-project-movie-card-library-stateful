2a10a6232bf03be6f9e456abedbd02bb14925cb0
luizfernando <luizfernandotic@gmail.com>
2020-04-30 12:05:32 -0300

    code climate 2

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -21,43 +21,33 @@ export default class AddMovie extends Component {
     this.setState({ [name]: value });
   }
 
+  const onClick  = this.props;
+  
+  const addmovie = () => {
+    onClick(this.state);
+    this.setState({ subtitle: '', title: '', imagePath: '', storyline: '', rating: 0, genre: 'action' });
+  };
+  const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+
   render() {
-    const { onClick } = this.props;
-    const addmovie = () => {
-      onClick(this.state);
-      this.setState({ subtitle: '', title: '', imagePath: '', storyline: '', rating: 0, genre: 'action' });
-    };
-    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-    return (
-      <div>
-        <label htmlFor>
-          Título
-          <input type="text" name="title" value={title} onChange={this.handleChange} />
-        </label>
-        <label htmlFor>
-          Subtítulo
-          <input type="text" name="subtitle" value={subtitle} onChange={this.handleChange} />
-        </label>
-        <label htmlFor>
-          ImagePath
-          <input type="text" name="imagePath" value={imagePath} onChange={this.handleChange} />
-        </label>
-        <label htmlFor>
-          Sinopse
-          <textarea type="text" name="storyline" value={storyline} onChange={this.handleChange} />
-        </label>
-        <label htmlFor>
-          Avaliação
-          <input type="number" name="rating" value={rating} onChange={this.handleChange} />
-        </label>
-        <select name="genre" value={genre}>
-          Gênero
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
-        <button type="button" onClick={addmovie}>Adicionar Filme</button>
-      </div>
+    return (<div>
+            <label htmlFor="titulo">Título</label>
+            <input type="text" name="title" value={title} onChange={this.handleChange} />
+            <label htmlFor="subtitulo">Subtítulo</label>
+            <input type="text" name="subtitle" value={subtitle} onChange={this.handleChange} />
+            <label htmlFor="imagepath">ImagePath</label>
+            <input type="text" name="imagePath" value={imagePath} onChange={this.handleChange} />
+            <label htmlFor="sinopse">Sinopse</label>
+            <textarea type="text" name="storyline" value={storyline} onChange={this.handleChange} />
+            <label htmlFor="avaliacao">Avaliação</label>
+            <input type="number" name="rating" value={rating} onChange={this.handleChange} />
+            <select name="genre" value={genre}>
+              <option value="action">Ação</option>
+              <option value="comedy">Comédia</option>
+              <option value="thriller">Suspense</option>
+            </select>
+            <button type="button" onClick={addmovie}>Adicionar Filme</button>
+            </div>
     );
   }
 }
