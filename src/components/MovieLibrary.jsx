52462bee59898bd621d1b5d18ed933d5cc4523ef
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-30 15:48:43 -0300

    codeclimate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 class MovieLibrary extends React.Component {
   render() {
-  };
+    return(
+      <p>MovieLibrary</p>
+    );
+  }
 }
 
 export default MovieLibrary;
