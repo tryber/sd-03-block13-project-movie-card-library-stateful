cfd97bb71205356bd06db5f0737ba2f1a0ca1d63
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-30 16:14:44 -0300

    codeclimate rev2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,14 +10,13 @@ class SearchBar extends React.Component {
     onSelectedGenreChange
     } = this.props;
     */
-    const {
-    searchText, onSearchTextChange,
-   } = this.props;
+    const { searchText, onSearchTextChange, } = this.props;
     return (
       <form>
         <label htmlFor="searchText"><span>Inclui o texto: </span>
-          <input type="text" name="searchBarText"
-          value={searchText} id="searchText" onChange = {onSearchTextChange} />
+          <input
+            type="text" name="searchBarText" value={searchText} id="searchText" onChange={onSearchTextChange} 
+          />
         </label>
       </form>
     );
