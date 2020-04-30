9fd449b3291db59f6f38850763eda32527872e88
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-04-30 20:34:11 -0300

    SearchBar Component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React from "react";
+
+class AddMovie extends React.Component {
+  createInput(text, id, type, value, callback) {
+    return (
+      <div>
+        <label htmlFor={id}>{text}</label>
+        <input
+          type={type}
+          id={id}
+          value={value}
+          onChange={(event, id) => callback(event, id)}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          {this.createInput("Título", "title", "text", searchText, onSearchTextChange)}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,54 @@
-// implement SearchBar component here
+import React from "react";
+
+const genreArray = [
+  { text: "Todos", value: "" },
+  { text: "Ação", value: "action" },
+  { text: "Comédia", value: "comedy" },
+  { text: "Suspense", value: "thriller" },
+];
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.props;
+    const {
+      onSearchTextChange,
+      onBookmarkedChange,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div>
+        <form>
+          <label htmlFor="text">Inclui o texto:</label>
+          <input
+            id="text"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
+          <label htmlFor="bookmarked">Mostrar somente favoritos</label>
+          <input
+            id="bookmarked"
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
+          <label htmlFor="genre">Filtrar por gênero</label>
+          <select
+            id="genre"
+            type="text"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
+            {genreArray.map(({ text, value }) => (
+              <option key={text} value={value}>
+                {text}
+              </option>
+            ))}
+          </select>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
