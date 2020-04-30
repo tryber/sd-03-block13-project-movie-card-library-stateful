ea1c14ac056596b50f8b66fd1d4f8a3fbbba2fbc
luis.campos <gucampos845@gmail.com>
2020-04-30 18:43:47 -0300

    Projeto movie card library stateful

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,60 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+const filtroMovies = (text, selectedGenre, saved, film) => {
+  const { title, subtitle, storyline, genre, bookmarked } = film;
+  return (title.includes(text) || subtitle.includes(text) || storyline.includes(text)) &&
+    (selectedGenre === '' ? true : genre === selectedGenre) &&
+    (saved ? bookmarked : true);
+};
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.addFilme = this.addFilme.bind(this);
+  }
+
+  onSearchChange(e) {
+    this.setState({
+      searchText: e.target.value,
+      bookmarkedOnly: true,
+      selectedGenre: e.target.value,
+    });
+  }
+
+  addFilme(novoFilme) {
+    this.setState((filme) => ({ movies: [...filme.movies, novoFilme] }));
+  }
+
+  render() {
+    const { searchText, selectedGenre, bookmarkedOnly, movies } = this.state;
+    return (
+      <div>
+        <SearchBar
+          onSelectedGenreChange={this.onSearchChange}
+          selectedGenre={selectedGenre}
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={this.onSearchChange}
+          onSearchTextChange={this.onSearchChange}
+          searchText={searchText}
+        />
+        <MovieList
+          movies={movies.filter((film) =>
+            filtroMovies(searchText, selectedGenre, bookmarkedOnly, film))}
+        />
+        <AddMovie onClick={this.addFilme} />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,29 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
+    } = this.props;
+    return (<div>
+      <form>
+        <label htmlFor="include">Inclui o texto
+            <input id="include" type="text" value={searchText} onChange={onSearchTextChange} />
+        </label><br />
+        <label htmlFor="f">Mostrar somente favoritos
+            <input id="f" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </label><br />
+        <label htmlFor="filter">Filtrar por gênero
+          <select id="filter" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select></label>
+      </form>
+    </div>);
+  }
+}
+
+export default SearchBar;
