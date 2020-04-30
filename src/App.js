704a702ddb439e5e7d2f9899c1ce7f6c8004b63a
renan.andreolle <renandreolle@hotmail.com>
2020-04-30 15:59:59 -0300

    MovieLibrary encaminhado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+// import MovieList from './components/MovieList';
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={movies} />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement MovieLibrary component here
+import React from 'react';
+import movies from '../data';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movies,
+    }
+  }
+    render() { 
+    return (
+      <div>
+        <MovieList movies={movies} />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
