8dfd123f9f6fef69165933f45552bffe09d610f7
Cardoso <lsc.cardosoo@gmail.com>
2020-04-30 15:13:53 -0300

    Refatorar função render do componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
@@ -10,25 +10,15 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange,
     } = this.props;
-    return (
+    return(
       <form>
         <label htmlFor="searchText">Inclui o texto:</label>
-        <input
-        type="text"
-        id="searchText"
-        value={searchText}
-        onChange={onSearchTextChange} />
-
+        <input type="text" id="searchText" value={searchText} onChange={onSearchTextChange} />
         <label htmlFor="favorits">Mostrar somente favoritos</label>
         <input type="checkbox"
-        id="favorits"
-        checked={bookmarkedOnly}
-        onChange={onBookmarkedChange} />
-
+        id="favorits" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         <label htmlFor="filterGenre">Filtrar por gênero</label>
-        <select id="filterGenre"
-        value={selectedGenre}
-        onChange={onSelectedGenreChange}>
+        <select id="filterGenre" value={selectedGenre} onChange={onSelectedGenreChange}>
           <option value="" >Todos</option>
           <option value="action" >Ação</option>
           <option value="comedy" >Comédia</option>
