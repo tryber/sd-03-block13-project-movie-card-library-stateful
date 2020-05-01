4e450af33169d71e0c665cbabda01b55d636903b
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 12:53:39 -0300

    Codeclimate check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../JSX_Styles/searchbarStyle.css'
+import '../JSX_Styles/searchbarStyle.css';
 
 class SearchBar extends React.Component {
   render() {
@@ -11,15 +11,17 @@ class SearchBar extends React.Component {
     return (
       <form className="searchForm">
         <label className="searchLabel" htmlFor="searchText">
-          Inclui o texto:<br/>
-          <input className="searchInput" id="searchText" type="text" value={searchText} onChange={onSearchTextChange} />
-        </label><br/>
+          Inclui o texto:<br/> 
+          <input className="searchInput" id="searchText" type="text" 
+          value={searchText} onChange={onSearchTextChange} />
+        </label><br/> 
         <label htmlFor="box">
-          <input className="checkbox_Input" id="box" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          <input className="checkbox_Input" id="box" type="checkbox" 
+          checked={bookmarkedOnly} onChange={onBookmarkedChange} />
           Mostrar somente favoritos:
-        </label><br/>
+        </label><br/> 
         <label className="genreLabel" htmlFor="genreSelect">
-          Filtrar por gênero:<br/>
+          Filtrar por gênero:<br/> 
           <select className="genreSelect" id="genreSelect" value={selectedGenre} onChange={onSelectedGenreChange}>
             <option value="">Todos</option>
             <option value="action">Ação</option>
