aa9b4f4944465f6fc822a1b200053951aba432de
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 15:08:24 -0300

    Codeclimate check

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,39 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  changeHandler = event => {
+    let { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div>
+        <form action=''>
+          <label htmlFor="title" >Título</label>
+          <input type="text" name="title" id="title" onChange={this.changeHandler} value={this.state.title}/>
+          {/* <label htmlFor="sub" >Subtítulo</label>
+          <input type="text" name="subtitle" id="sub" onChange={this.update} value={this.state.subtitle}/> */}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
