6284fa8e0b23facbd94c0a7c8f318e8599874611
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 14:28:40 -0300

    Codeclimate check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,37 +3,31 @@ import '../JSX_Styles/searchbarStyle.css';
 import Data from './genreData.js';
 
 const SearchBar = (props) => {
-    const {
-      bookmarkedOnly, onBookmarkedChange, onSearchTextChange, onSelectedGenreChange,
-      searchText, selectedGenre,
-    } = props;
+  const {
+    bookmarkedOnly, onBookmarkedChange, onSearchTextChange, onSelectedGenreChange,
+    searchText, selectedGenre,
+  } = props;
 
   return (
     <form className="searchForm">
-      <label className="searchLabel" htmlFor="s.t">
-        Inclui o texto:<br />
+      <label className="searchLabel" htmlFor="s.t">Inclui o texto:</label><br />
         <input
           value={searchText} onChange={onSearchTextChange}
           className="searchInput" id="s.t" type="text"
         />
-      </label><br />
-      <label htmlFor="c.b">
+      <label htmlFor="c.b"><br />
         <input
           checked={bookmarkedOnly} onChange={onBookmarkedChange}
           className="checkbox_Input" id="c.b" type="checkbox"
-        />
-        Mostrar somente favoritos:
-      </label><br />
-      <label className="genreLabel" htmlFor="g.s">
-        Filtrar por gênero:<br />
-        <select className="genreSel" id="g.s" 
+        />Mostrar somente favoritos:</label>
+      <label className="genreLabel" htmlFor="g.s">Filtrar por gênero:</label>
+        <select 
+          className="genreSel" id="g.s" 
           value={selectedGenre} onChange={onSelectedGenreChange}
-        >
-          {Data.map((e) => <option key={e.text} value={e.value}>{e.text}</option>)}
+        >{Data.map((e) => <option key={e.text} value={e.value}>{e.text}</option>)}
         </select>
-      </label>
     </form>
   );
-}
+};
 
 export default SearchBar;
