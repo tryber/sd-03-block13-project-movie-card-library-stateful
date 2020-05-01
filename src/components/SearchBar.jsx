1c66ac3aa515d7e3037340e2926911db9084e392
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 12:50:06 -0300

    Search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,40 +1,33 @@
-// implement SearchBar component here
-import React, { Component } from 'react';
-import SearchbarInput from './SearchBarInput';
-import SearchBarCheckbox from './SearchBarCheckbox';
+import React from 'react';
+import '../JSX_Styles/searchbarStyle.css'
 
-export class SearchBar extends Component {
-  // constructor(props){
-  //   super(props)
-
-  //   this.state = {
-  //     searchText: '',
-  //   }
-  // }
-
-  //   searchText, uma string
-  // onSearchTextChange, uma callback
-  // bookmarkedOnly, um boolean
-  // onBookmarkedChange, uma callback
-  // selectedGenre, uma string
-  // onSelectedGenreChange, uma callback
-
+class SearchBar extends React.Component {
   render() {
-    // const {
-    //   searchText,
-    //   onSearchTextChange,
-    //   // bookmarkedOnly,
-    //   // selectedGenre,
-    //   // onSelectedGenreChange,
-    // } = this.props;
+    const {
+      bookmarkedOnly, onBookmarkedChange, onSearchTextChange, onSelectedGenreChange,
+      searchText, selectedGenre,
+    } = this.props;
 
     return (
-      <div>
-        <form className="searchForm">
-          <SearchbarInput />
-          <SearchBarCheckbox />
-        </form>
-      </div>
+      <form className="searchForm">
+        <label className="searchLabel" htmlFor="searchText">
+          Inclui o texto:<br/>
+          <input className="searchInput" id="searchText" type="text" value={searchText} onChange={onSearchTextChange} />
+        </label><br/>
+        <label htmlFor="box">
+          <input className="checkbox_Input" id="box" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          Mostrar somente favoritos:
+        </label><br/>
+        <label className="genreLabel" htmlFor="genreSelect">
+          Filtrar por gênero:<br/>
+          <select className="genreSelect" id="genreSelect" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
