b859caa25489d5184f5491d3b7b50112b654b5d8
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 16:42:44 -0300

    Codeclimate check

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,33 +14,61 @@ class AddMovie extends Component {
     super(props);
 
     this.state = initialState;
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(name) {
-    return ( ({ target: { value } }) => {
+    return ({ target: { value } }) => {
       const nextState = {};
       if (name === 'rating') nextState[name] = Number(value);
       else nextState[name] = value;
 
       this.setState(nextState);
-    });
+    };
   }
-
 
   render() {
     // const { onClick } = this.props;
     return (
       <div>
-        <form action=''>
-          <label htmlFor="title" >Título</label>
-          <input type="text" name="title" id="title" onChange={this.handleChange('title')} value={this.state.title}/>
-          <label htmlFor="sub" >Subtítulo</label>
-          <input type="text" name="subtitle" id="sub" onChange={this.handleChange('subtitle')} value={this.state.subtitle}/>
-          <label htmlFor="img" >Imagem</label>
-          <input type="text" alt="teste" name="image" id="img" onChange={this.handleChange('imagePath')} value={this.state.imagePath}/>
-          <label htmlFor="sin" >Sinopse</label>
-          <textarea type="textarea" name="storyline" id="sin" onChange={this.handleChange('storyline')} value={this.state.storyline}/>
+        <form action="">
+          <label htmlFor="title">Título</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={this.handleChange("title")}
+            value={this.state.title}
+          />
+          <br />
+          <label htmlFor="sub">Subtítulo</label>
+          <input
+            type="text"
+            name="subtitle"
+            id="sub"
+            onChange={this.handleChange("subtitle")}
+            value={this.state.subtitle}
+          />
+          <br />
+          <label htmlFor="img">Imagem</label>
+          <input
+            type="text"
+            alt="teste"
+            name="image"
+            id="img"
+            onChange={this.handleChange("imagePath")}
+            value={this.state.imagePath}
+          />
+          <br />
+          <label htmlFor="sin">Sinopse</label>
+          <textarea
+            type="textarea"
+            name="storyline"
+            id="sin"
+            onChange={this.handleChange("storyline")}
+            value={this.state.storyline}
+          />
+          <br />
         </form>
       </div>
     );
