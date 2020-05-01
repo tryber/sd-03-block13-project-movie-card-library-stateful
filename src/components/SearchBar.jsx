625e4f86be1a0862e36d9e3c8af7d5d1cf29a9df
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 13:13:24 -0300

    Codeclimate check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../JSX_Styles/searchbarStyle.css';
+import Data from './genreData'
 
 class SearchBar extends React.Component {
   render() {
@@ -11,22 +12,19 @@ class SearchBar extends React.Component {
     return (
       <form className="searchForm">
         <label className="searchLabel" htmlFor="searchText">
-          Inclui o texto:<br/> 
+          Inclui o texto:<br />
           <input className="searchInput" id="searchText" type="text" 
           value={searchText} onChange={onSearchTextChange} />
-        </label><br/> 
+        </label><br />
         <label htmlFor="box">
           <input className="checkbox_Input" id="box" type="checkbox" 
           checked={bookmarkedOnly} onChange={onBookmarkedChange} />
           Mostrar somente favoritos:
-        </label><br/> 
+        </label><br />
         <label className="genreLabel" htmlFor="genreSelect">
-          Filtrar por gênero:<br/> 
+          Filtrar por gênero:<br />
           <select className="genreSelect" id="genreSelect" value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+            {Data.map(info => <option key={Math.random() * 1000} value={info.value}>{info.genre}</option>)}
           </select>
         </label>
       </form>
---
+++ src/components/genreData.jsx
@@ -0,0 +1,20 @@
+const data = [
+  {
+    genre: 'Todos',
+    value: '',
+  },
+  {
+    genre: 'Ação',
+    value: 'action',
+  },
+  {
+    genre: 'Comédia',
+    value: 'comedy',
+  },
+  {
+    genre: 'Suspense',
+    value: 'thriller',
+  },
+]
+
+export default data;
