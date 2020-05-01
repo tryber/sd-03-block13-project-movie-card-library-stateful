5b43636b4c2725962233b7cf808965169f9c0d1a
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 19:23:38 -0300

    Codeclimate check

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Genre from './genreData'
 
 const initialState = {
   genre: 'action',
@@ -15,20 +16,27 @@ class AddMovie extends Component {
 
     this.state = initialState;
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmitClick = this.handleSubmitClick.bind(this);
   }
 
   handleChange(name) {
     return ({ target: { value } }) => {
       const nextState = {};
-      if (name === 'rating') nextState[name] = Number(value);
-      else nextState[name] = value;
-
+      name === 'rating' ? (nextState[name] = Number(value)) : (nextState[name] = value);
       this.setState(nextState);
     };
   }
 
+  reset() { this.setState(initialState); }
+
+  handleSubmitClick(event) {
+    if (event && event.preventDefault) event.preventDefault();
+    this.props.onClick(this.state);
+    this.reset();
+  }
+
+
   render() {
-    // const { onClick } = this.props;
     return (
       <div>
         <form action="">
@@ -44,6 +52,14 @@ class AddMovie extends Component {
           <label htmlFor="sin">Sinopse</label>
           <textarea type="textarea" name="storyline" id="sin" onChange={this.handleChange('storyline')} value={this.state.storyline} />
           <br />
+          <label htmlFor="rat">Avaliação</label>
+          <input type="number" name="storyline" id="rat" onChange={this.handleChange('rating')} value={this.state.rating} />
+          <br />
+          <label htmlFor="gen">Gênero</label>
+          <select id="gen" onChange={this.handleChange('genre')} value={this.state.genre}>
+            {Genre.slice(1,4).map(e => <option key={e.text} value={e.value}>{e.text}</option>)}
+          </select>
+          <button type={'submit'} onClick={this.handleSubmitClick}>Adicionar filme</button>
         </form>
       </div>
     );
