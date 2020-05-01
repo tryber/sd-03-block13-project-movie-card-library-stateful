c48e6d1170813a31f5a250b8cd69a51c6f97ae19
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-30 22:29:00 -0300

    Codeclimate check

--- src/components/SearchBarCheckbox.jsx
+++ src/components/SearchBarCheckbox.jsx
@@ -3,11 +3,19 @@ import '../JSX_Styles/searchbarStyle.css';
 
 export class SearchBarCheckbox extends Component {
   render() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <div>
         <form>
-          <label className="searchForm" htmlFor="checkbox">
-            <input style={{ marginRight: "10px" }} type="checkBox" name="" id="checkbox" />
+          <label className='searchForm' htmlFor='checkbox'>
+            <input
+              onChange={onBookmarkedChange}
+              checked={bookmarkedOnly}
+              style={{ marginRight: "10px" }}
+              type='checkBox'
+              name=''
+              id='checkbox'
+            />
             Mostrar somente favoritos
           </label>
         </form>
