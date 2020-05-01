e335c6ebf20f096cc8b91576210674ee9473446d
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-01 14:31:58 -0300

    Codeclimate check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,21 +11,21 @@ const SearchBar = (props) => {
   return (
     <form className="searchForm">
       <label className="searchLabel" htmlFor="s.t">Inclui o texto:</label><br />
-        <input
-          value={searchText} onChange={onSearchTextChange}
-          className="searchInput" id="s.t" type="text"
-        />
+      <input
+        value={searchText} onChange={onSearchTextChange}
+        className="searchInput" id="s.t" type="text"
+      />
       <label htmlFor="c.b"><br />
         <input
           checked={bookmarkedOnly} onChange={onBookmarkedChange}
           className="checkbox_Input" id="c.b" type="checkbox"
         />Mostrar somente favoritos:</label>
       <label className="genreLabel" htmlFor="g.s">Filtrar por gênero:</label>
-        <select 
-          className="genreSel" id="g.s" 
+      <select
+          className="genreSel" id="g.s"
           value={selectedGenre} onChange={onSelectedGenreChange}
         >{Data.map((e) => <option key={e.text} value={e.value}>{e.text}</option>)}
-        </select>
+      </select>
     </form>
   );
 };
