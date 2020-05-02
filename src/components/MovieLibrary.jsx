16ec5de23d5217f8f8d237447070742bdb8bd382
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-02 11:44:17 -0300

    Codeclimate check

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -8,41 +8,44 @@ import AddMovie from './AddMovie';
 
 export class MovieLibrary extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       searchText: '',
-       bookmarkedOnly: false,
-       selectedGenre: '',
-       movies: this.props.movies,
-    }
-    this.onSearchTextChange = this.onSearchTextChange.bind(this)
-    this.onBookmarkedChange = this.onBookmarkedChange.bind(this)
-    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this)
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
   }
 
-  onSearchTextChange({ target }) { 
-    this.setState({ searchText: target.value }); 
+  onSearchTextChange({ target }) {
+    this.setState({ searchText: target.value });
   }
 
-  onBookmarkedChange({ target }) { 
-    this.setState({ bookmarkedOnly: target.checked }); 
+  onBookmarkedChange({ target }) {
+    this.setState({ bookmarkedOnly: target.checked });
   }
 
-  onSelectedGenreChange({ target }) { 
-    this.setState({ selectedGenre: target.value }); 
+  onSelectedGenreChange({ target }) {
+    this.setState({ selectedGenre: target.value });
   }
-  
+
   render() {
-
     return (
       <div>
-        <SearchBar 
-          searchText={this.state.searchText} onSearchTextChange={this.onSearchTextChange}
-          onBookmarkedChange={this.onBookmarkedChange} bookmarkedOnly={this.state.bookmarkedOnly}
-          selectedGenre={this.state.selectedGenre} onSelectedGenreChange={this.onSelectedGenreChange}
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          onBookmarkedChange={this.onBookmarkedChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={MovieData}/>
+        <MovieList movies={MovieData} />
         <AddMovie />
       </div>
     );
