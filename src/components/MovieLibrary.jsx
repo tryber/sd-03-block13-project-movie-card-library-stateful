d79e54a9e5b6d6624bee6748f9278765f087d693
DevT <aguiarthiag@gmail.com>
2020-05-03 17:22:36 -0300

    Iniciando o projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,70 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      movies: this.props.movies,
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    }
+    this.handleBmChange = this.handleBmChange.bind(this);
+    this.movieFilter = this.movieFilter.bind(this);
+  }
+
+  handleBmChange() {
+    this.setState({bookmarkedOnly: !this.state.bookmarkedOnly})
+  }
+
+  bmCheck (movie) {
+    if(this.state.bookmarkedOnly === false || movie.bookmarked === true){ //checar se apenas um check movie.bookmarked === true substitui essa linha
+      return movie;
+    }
+    return false;
+  }
+
+  genreCheck(movie) {
+    if(!this.state.selectedGenre || 
+    (this.state.selectedGenre && this.state.selectedGenre === movie.genre)){
+      return movie;
+    }
+    return false;
+  }
+
+  textCheck(movie) {
+    if(movie.title.includes(this.state.searchText) ||
+    movie.subtitle.includes(this.state.searchText) ||
+    movie.storyline.includes(this.state.searchText)){
+      return movie;
+    }
+    return false;
+  }
+
+  movieFilter(movies) {
+    const filtered = movies.filter(movie =>
+      this.bmCheck(movie) && this.genreCheck(movie) && this.textCheck(movie)
+    )
+    return filtered;
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar 
+        searchText={this.state.searchText}
+        onSearchTextChange={(e) => this.setState({ searchText: e.target.value })}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        onBookmarkedChange={this.handleBmChange}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenreChange={(e) => this.setState({ selectedGenre: e.target.value })}
+        />
+        <MovieList movies={this.movieFilter(this.state.movies)} />
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,33 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render () {
+    return (
+      <form>
+
+        <label htmlFor='text'>Inclui o texto:</label>
+        <input name='text' type='text' 
+        onChange={this.props.onSearchTextChange} value={this.props.searchText}></input>
+
+        <br />
+
+        <label htmlFor='bookmarked'>Mostrar somente favoritos</label>
+        <input name='bookmarked' type='checkbox' checked={this.props.bookmarkedOnly}
+        onChange={this.props.onBookmarkedChange}></input>
+
+        <br />
+        
+        <label htmlFor='genre'>Filtrar por gênero</label>
+        <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
