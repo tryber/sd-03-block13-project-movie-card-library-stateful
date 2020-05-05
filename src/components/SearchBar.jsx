33c07fa7dea27f3297b80811c96d31e2ed95fdfa
gabryel <gabryel.ryba@gmail.com>
2020-05-05 14:30:03 -0300

    SearchBar component refactored

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,34 +1,65 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  textSearchRenderer() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label htmlFor="textInput">
+        Inclui o texto
+        <input
+          type="text"
+          name="movieText"
+          id="textInput"
+          value={searchText}
+          onChange={onSearchTextChange}
+        />
+      </label>
+    );
+  }
+
+  bookmarkCheckRenderer() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label htmlFor="checkBox">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          name="movieCheckBox"
+          id="checkBox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        />
+      </label>
+    );
+  }
+
+  genreSelectorRenderer() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="select">
+        Filtrar por gênero
+        <select
+          name="genreSelect"
+          id="select"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
     return (
       <form action="get" className="searchBar">
-        <label htmlFor="textInput">
-          Inclui o texto
-          <input type="text" name="movieText" id="textInput" value={searchText} onChange={onSearchTextChange} />
-        </label>
-        <label htmlFor="checkBox">
-          Mostrar somente favoritos
-          <input type="checkbox" name="movieCheckBox" id="checkBox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        </label>
-        <label htmlFor="select">
-          Filtrar por gênero
-          <select name="genreSelect" id="select" value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
+        {this.textSearchRenderer()}
+        {this.bookmarkCheckRenderer()}
+        {this.genreSelectorRenderer()}
       </form>
     );
   }
