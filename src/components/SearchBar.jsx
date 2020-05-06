b5c9315216a86b0250ec9f34d6a94c82481a6542
Mateus Moreira <ashuraw@hotmail.com>
2020-05-06 18:28:37 -0300

    3.1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,43 +1,34 @@
 import React from 'react';
 
+const genreArray = [
+    { text: 'Todos', value: '' },
+    { text: 'Ação', value: 'action' },
+    { text: 'Comédia', value: 'comedy' },
+    { text: 'Suspense', value: 'thriller' },
+  ];
+
 export default class SearchBar extends React.Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
     } = this.props;
+
     return (
       <div>
         <form>
-          <label htmlFor="">Inclui o texto:</label>
-          <input
-            id="text"
-            type="text"
-            value={searchText}
-            onChange={onSearchTextChange}
+          <label htmlFor="text">Inclui o texto:</label>
+          <input id="text" type="text" value={searchText} onChange={onSearchTextChange}
           />
           <label htmlFor="checkbox">Mostrar somente favoritos</label>
-          <input
-            id="checkbox"
-            type="checkbox"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}
           />
           <label htmlFor="genre">Filtrar por gênero</label>
-          <select
-            name=""
-            id="genre"
-            value={selectedGenre}
-            onChange={onSelectedGenreChange}
-          >
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+          <select value={selectedGenre} onChange={onSelectedGenreChange}>
+            {genreArray.map(({ text, value }) => (
+              <option key={text} value={value}>
+                {text}
+              </option>
+            ))}
           </select>
         </form>
       </div>
