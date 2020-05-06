272fee5945a769ae21d14ebd103b91c72b7dc9a4
Mateus Moreira <ashuraw@hotmail.com>
2020-05-05 22:23:26 -0300

    2.1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default class SearchBar extends React.Component {
   render() {
     return (
-        <form>
-            <input type="text" name="" id=""/>
-            <input type="checkbox" name="" id=""/>
-            <select name="" id=""></select>
-        </form>
+      <form>
+        <input type="text" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <select name="" id=""></select>
+      </form>
     );
   }
 }
