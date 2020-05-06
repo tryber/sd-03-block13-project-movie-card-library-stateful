adea0f1469a52de1a182e2d2e162cb7416b906ab
Rhian Castro <rhi.castro@gmail.com>
2020-05-06 19:28:11 -0300

    fixed quotes for codeclimate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import MovieLibrary from "./components/MovieLibrary";
-import Data from "./data";
-import "./App.css";
-import Header from "./components/Header";
+import React from 'react';
+import MovieLibrary from './components/MovieLibrary';
+import Data from './data';
+import './App.css';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
