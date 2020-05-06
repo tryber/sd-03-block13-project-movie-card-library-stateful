7e39da50e4023e69b9b991dc192b4a5fd53835f0
Thiago Tavares <thiagooo15@gmail.com>
2020-05-06 17:32:57 -0300

    searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 
 export class SearchBar extends Component {
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.props;
+    const { onSearchTextChange, onBookmarkedChange, onSelectedGenreChange } = this.props;
+
     return (
       <div>
-        <h1>vasio</h1>
+        <form>
+          <label>Inclui o texto:</label>
+          <input type="text" value={searchText} onChange={onSearchTextChange} />
+          <label>Mostrar somente favoritos</label>
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          <label>Filtrar por gênero</label>
+          <select value={selectedGenre} onChange={onSelectedGenreChange} />
+        </form>
       </div>
     );
   }
