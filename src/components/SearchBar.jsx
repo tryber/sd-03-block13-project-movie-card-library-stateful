d8452d3bf89e0cfc8bc5a6d21c8c300ff8ed305d
Mateus Moreira <ashuraw@hotmail.com>
2020-05-05 22:06:21 -0300

    starting

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
-    render()
+  render() {
+    return (
+      <header>
+        <h1 className="movie-card-header">Movie Cards Library</h1>
+      </header>
+    );
   }
+}
