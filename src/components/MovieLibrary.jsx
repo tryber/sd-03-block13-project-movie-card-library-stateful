a0efddf41e0ae4deabc079e4b13cf95b7907f286
mirellaalves <mirella.alves@gmail.com>
2020-05-06 13:16:07 -0300

    Excluding "bind" on MovieLibrary.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,10 +14,6 @@ class MovieLibrary extends Component {
     }
   }
 
-  this.insertSearch = this.insertSearch.bind(this);
-  this.changeBookMark = this.changeBookMark.bind(this);
-  this.selectGenre = this.selectGenre.bind(this);
-
   insertSearch = (value) => {
     this.setState((state) => ({
       searchText: `${state.searchText}${value}`,
