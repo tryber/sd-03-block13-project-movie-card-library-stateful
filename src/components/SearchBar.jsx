fd834d21e658b8c10639e5d164a6e6672939c623
mirellaalves <mirella.alves@gmail.com>
2020-05-06 00:54:47 -0300

    Code climate issues.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
 const SearchBar = () => {
-  const {
-    searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
-  } = this.props;
+  const { searchText, onSearchTextChange,
+    bookmarkedOnly, onBookmarkedChange,
+    selectedGenre, onSelectedGenreChange, } = this.props;
 
   return (
     <form>
       <label htmlFor="searchText">Inclui o texto:
-      <input type="text" id="searchText" name="searchText"
-        value={searchText} onChange={onSearchTextChange} />
+      <input
+        type="text" id="searchText" name="searchText"
+        value={searchText} onChange={onSearchTextChange}
+      />
       </label>
       <label htmlFor="bookmarkedOnly">Mostrar somente favoritos
-      <input type="checkbox" id="bookmarkedOnly" name="bookmarkedOnly"
-        checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+      <input
+        type="checkbox" id="bookmarkedOnly" name="bookmarkedOnly"
+        checked={bookmarkedOnly} onChange={onBookmarkedChange}
+      />
       </label>
       <label htmlFor="selectedGenre">Filtrar por gênero
-      <select id="selectedGenre" name="selectedGenre"
-        value={selectedGenre} onChange={onSelectedGenreChange} />
+      <select
+        id="selectedGenre" name="selectedGenre"
+        value={selectedGenre} onChange={onSelectedGenreChange}
+      />
         <option value="">Todos</option>
         <option value="action">Ação</option>
         <option value="comedy">Comédia</option>
