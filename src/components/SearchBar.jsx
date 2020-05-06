362ac5a94687981880a88e559851f891985c4bbe
Thiago Tavares <thiagooo15@gmail.com>
2020-05-06 17:40:27 -0300

    searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,12 +8,17 @@ export class SearchBar extends Component {
     return (
       <div>
         <form>
-          <label>Inclui o texto:</label>
+          <label htmlFor="text">Inclui o texto:</label>
           <input type="text" value={searchText} onChange={onSearchTextChange} />
           <label>Mostrar somente favoritos</label>
           <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
           <label>Filtrar por gênero</label>
-          <select value={selectedGenre} onChange={onSelectedGenreChange} />
+          <select value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
         </form>
       </div>
     );
