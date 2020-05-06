1ec500b763e556ee1ebc0703bf6365aac19d8863
mirellaalves <mirella.alves@gmail.com>
2020-05-06 13:11:24 -0300

    Keep working on SearchBar component and redering.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,23 +1,51 @@
-// implement MovieLibrary component here
-// daqui pra baixo, inserido conforme "ANTES DE COMEÇAR O PROJETO", das instruções do projeto.
 import React, { Component } from 'react';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+    }
+  }
+
+  this.insertSearch = this.insertSearch.bind(this);
+  this.changeBookMark = this.changeBookMark.bind(this);
+  this.selectGenre = this.selectGenre.bind(this);
+
+  insertSearch = (value) => {
+    this.setState((state) => ({
+      searchText: `${state.searchText}${value}`,
+    }));
+  }
+
+  changeBookMark = () => {
+    this.setState({ bookmarkedOnly: true, });
+  }
+
+  selectGenre = (value) => {
+    this.setState({selectedGenre: value,});
+  }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <SearchBar
+        searchText={this.state.searchText}
+        onSearchTextChange={() => this.insertSearch()}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        onBookmarkedChange={() => this.changeBookMark()}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenreChange={() => this.selectGenre()}
+        />
+        {/* <MovieList movies={this.props.movies} />
+        <AddMovie /> */}
       </div>
     );
   }
