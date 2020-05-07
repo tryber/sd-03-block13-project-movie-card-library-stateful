895fe7057de0e64227601b1178af664d94a7a591
Mateus Moreira <ashuraw@hotmail.com>
2020-05-06 23:38:05 -0300

    3.5.1

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 const genreArray = [
-    { text: 'Ação', value: 'action' },
-    { text: 'Comédia', value: 'comedy' },
-    { text: 'Suspense', value: 'thriller' },
+  { text: 'Ação', value: 'action' },
+  { text: 'Comédia', value: 'comedy' },
+  { text: 'Suspense', value: 'thriller' },
 ];
 
 export class AddMovie extends Component {
@@ -17,26 +17,36 @@ export class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+      const {evento} = event
+    this.setState({ [evento]: event.target.value });
+  }
+  
 
   render() {
     return (
       <div>
         <form>
           <label htmlFor="Título">Título</label>
-          <input type="text" name="" id="" value={this.state.title} />
+          <input type="text" value={this.state.title} onChange={this.handleChange} />
           <label htmlFor="Subtítulo">Subtítulo</label>
-          <input type="text" name="" id="" value={this.state.subtitle} />
+          <input type="text" value={this.state.subtitle} />
           <label htmlFor="Imagem">Imagem</label>
-          <input type="text" name="" id="" value={this.state.imagePath} />
+          <input type="text" value={this.state.imagePath} />
           <label htmlFor="Sinopse">Sinopse</label>
-          <textarea name="" id="" cols="30" rows="10" value={this.state.storyline} />
+          <textarea cols="30" rows="10" value={this.state.storyline} />
           <label htmlFor="Avaliação">Avaliação</label>
-          <input type="number" name="" id="" value={this.state.rating} />
+          <input type="number" value={this.state.rating} />
           <label htmlFor="Gênero">Gênero</label>
-          <select name="" id="" value={this.state.genre}>
+          <select value={this.state.genre}>
             {genreArray.map(({ text, value }) => (
-              <option key={text} value={value}>{text}</option>
+              <option key={text} value={value}>
+                {text}
+              </option>
             ))}
           </select>
           <button>Adicionar filme</button>
