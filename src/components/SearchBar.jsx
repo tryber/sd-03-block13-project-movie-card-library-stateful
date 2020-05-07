3784feb78abd77b1501c40434ad0ea9d8b5456e9
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-07 15:26:48 -0300

    Adjusting code climate and verifying if SearchBar.jsx still pass on all tests

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor(props){
+  /*c onstructor(props) {
     super(props);
-  }
-  render(){
-    return(
+  } */
+  render() {
+    return (
       <form>
         <div>
-          <label for='textSearch'>Inclui o texto:</label>
-          <input id='textSearch' type='text' value={this.props.searchText} onChange={this.props.onSearchTextChange}></input>
+          <label htmlFor="textSearch">Inclui o texto:</label>
+          <input id="textSearch" type="text" value={this.props.searchText} 
+          onChange={this.props.onSearchTextChange} />
         </div>
         <div>
-          <label for='selectFavorites'>Mostrar somente favoritos</label>
-          <input id='selectFavorites' type='checkbox' checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}></input>
+          <label htmlFor="selectFavorites">Mostrar somente favoritos</label>
+          <input id="selectFavorites" type="checkbox" checked={this.props.bookmarkedOnly} 
+          onChange={this.props.onBookmarkedChange} />
         </div>
         <div>
-          <label for='filter'>Filtrar por gênero</label>
-          <select id ='filter' value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-            <option value=''>Todos</option>
-            <option value='action'>Ação</option>
-            <option value='comedy'>Comédia</option>
-            <option value='thriller'>Suspense</option>
+          <label htmlFor="filter">Filtrar por gênero</label>
+          <select id="filter" value={this.props.selectedGenre} 
+          onChange={this.props.onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
           </select>
         </div>
       </form>
-    )
+    );
   }
 }
 
