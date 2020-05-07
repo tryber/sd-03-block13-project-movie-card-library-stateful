859b9aeb7310a3d3c63eb862304e230a6ec65ab1
Lourenço <loulage@gmail.com>
2020-05-07 19:01:10 -0300

    Rendered SearchBar&AddMovie on MovieLibrary component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,49 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        searchText:'',
+        bookmarkedOnly: false,
+        selectedGenre: '',
+        movies: this.props.movies,
+    };
+    this.onClick = this.onClick.bind(this);
+}
+
+  onChange(event, key) {
+    this.setState({ [key]: (key === 'bookmarkedOnly' ? event.target.checked : event.target.value) });
+  }
+
+  onClick(newMovie) {
+    const { movies } = this.state;
+    this.setState({ movies: [...movies, newMovie] });
+  }
+
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={searchText}
+          onSearchTextChange={(event) => this.onChange(event, 'searchText')}
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={(event) => this.onChange(event, 'bookmarkedOnly')}
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={(event) => this.onChange(event, 'selectedGenre')}
+        />
+        <AddMovie onClick={this.onClick} />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
