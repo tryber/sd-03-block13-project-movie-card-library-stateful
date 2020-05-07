8ff2ab18d1999c1c86c842d9b120afd8256a5cef
Lourenço <loulage@gmail.com>
2020-05-07 18:33:15 -0300

    CC searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,23 +10,17 @@ class SearchBar extends Component {
     } = this.props;
     return (
       <form>
-        <label htmlFor="input">
-          Inclui o texto:
-          <input type="text" value={searchText} onChange={onSearchTextChange} />
-        </label>
-        <label htmlFor="input">
-          Mostrar somente favoritos
-          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        </label>
-        <label htmlFor="input">
-          Filtrar por gênero
-          <select value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
+        <label htmlFor="input">Inclui o texto: </label>
+        <input type="text" value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor="input">Mostrar somente favoritos</label>
+        <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        <label htmlFor="input">Filtrar por gênero</label>
+        <select value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
       </form>
     );
   }
