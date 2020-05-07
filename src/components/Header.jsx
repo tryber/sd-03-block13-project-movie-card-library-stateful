cb24235988e7d70f1e574183c42a1f8232988717
felipedoss <felipecaetano25.1996@gmail.com>
2020-05-06 23:52:14 -0300

    f

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.c = <h1 className="page-title">Movie Cards Library</h1>;
+    this.f = 4;
+    console.log(this);
+    this.user = {
+      firstName: 'Harper',
+      lastName: 'Perez',
+    };
+ 
+  }
+
+
   render() {
     return (
       <header className="movie-card-header">
