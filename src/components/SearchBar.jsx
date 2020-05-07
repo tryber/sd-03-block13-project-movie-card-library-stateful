aa95db2c73adbc70eb5dadcb5672d0231e4a4101
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-07 15:31:13 -0300

    Adjusting code climate and verifying if SearchBar.jsx still pass on all tests

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  /*c onstructor(props) {
+  /* constructor(props) {
     super(props);
   } */
   render() {
@@ -9,18 +9,29 @@ class SearchBar extends React.Component {
       <form>
         <div>
           <label htmlFor="textSearch">Inclui o texto:</label>
-          <input id="textSearch" type="text" value={this.props.searchText} 
-          onChange={this.props.onSearchTextChange} />
+          <input
+            id="textSearch"
+            type="text"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange}
+          />
         </div>
         <div>
           <label htmlFor="selectFavorites">Mostrar somente favoritos</label>
-          <input id="selectFavorites" type="checkbox" checked={this.props.bookmarkedOnly} 
-          onChange={this.props.onBookmarkedChange} />
+          <input
+            id="selectFavorites"
+            type="checkbox"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />
         </div>
         <div>
           <label htmlFor="filter">Filtrar por gênero</label>
-          <select id="filter" value={this.props.selectedGenre} 
-          onChange={this.props.onSelectedGenreChange}>
+          <select
+            id="filter"
+            value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}
+          >
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
