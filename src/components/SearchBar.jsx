8a1fca20aed0e103e249a0adca641a81e53e4815
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-07 15:43:30 -0300

    Adjusting Code Climate and checking if new refactoring is allowed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,33 +5,21 @@ class SearchBar extends React.Component {
     super(props);
   } */
   render() {
+    const {searchText, onSearchTextChange, bookmarkedOnly, 
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props
     return (
       <form>
         <div>
           <label htmlFor="textSearch">Inclui o texto:</label>
-          <input
-            id="textSearch"
-            type="text"
-            value={this.props.searchText}
-            onChange={this.props.onSearchTextChange}
-          />
+          <input id="textSearch" type="text" value={searchText} onChange={onSearchTextChange} />
         </div>
         <div>
           <label htmlFor="selectFavorites">Mostrar somente favoritos</label>
-          <input
-            id="selectFavorites"
-            type="checkbox"
-            checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}
-          />
+          <input id="selectFavorites" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </div>
         <div>
           <label htmlFor="filter">Filtrar por gênero</label>
-          <select
-            id="filter"
-            value={this.props.selectedGenre}
-            onChange={this.props.onSelectedGenreChange}
-          >
+          <select id="filter" value={selectedGenre} onChange={onSelectedGenreChange} >
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
