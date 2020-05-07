d402d738a630bbb0b84aa63553c1cef5400119e3
Mateus Moreira <ashuraw@hotmail.com>
2020-05-07 13:16:40 -0300

    4.2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,16 +4,16 @@ import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies:  props.movies,
+    };
+  }
   render() {
-    constructor(props) {
-      super(props);
-      this.state = {
-        searchText: '',
-        bookmarkedOnly: false,
-        selectedGenre: '',
-        movies:  props.movies,
-      };
-    }
     return (
       <div>
         <SearchBar />
