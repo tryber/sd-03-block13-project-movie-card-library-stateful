fa650bcede9c973397162e2820663c175978d356
Lourenço <loulage@gmail.com>
2020-05-07 19:06:33 -0300

    called MovieLibrary on APP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
+import data from './data';
 
 import Header from './components/Header';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={data} />
     </div>
   );
 }
