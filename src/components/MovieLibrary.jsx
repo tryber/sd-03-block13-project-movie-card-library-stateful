c19f3d2efb6084d5d2415a8325070dea19e78514
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-05-13 05:38:02 -0300

    SearchBarP3

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -9,9 +9,11 @@ export default class MovieLibrary extends React.Component {
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
+      selectedGenre: 'action'
     };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this)
   }
 
   onSearchTextChange(event) {
@@ -24,6 +26,11 @@ export default class MovieLibrary extends React.Component {
     this.setState({ bookmarkedOnly: input.value });
   }
 
+  onSelectedGenreChange(event) {
+    const input = event.target;
+    this.setState({ selectedGenre: input.value });
+  }
+
   render() {
     return (
       <div>
@@ -32,6 +39,8 @@ export default class MovieLibrary extends React.Component {
           onSearchTextChange={this.onSearchTextChange}
           bookmarkedOnly={this.state.bookmarkedOnly}
           onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
         />
         <MovieList movies={this.props.movies} />
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,6 +17,17 @@ export default class SearchBar extends React.Component {
           checked={this.props.bookmarkedOnly}
           onChange={this.props.onBookmarkedChange}
         />
+        <label htmlFor="Text">Filtrar por gênero</label>
+        <select
+          name="Generos"
+          value={this.props.selectedGenre}
+          onChange={this.props.onSelectedGenreChange}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
       </form>
     );
   }
