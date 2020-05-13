da7387d8cd00ff66bf548f304725226f0f93308f
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-05-13 05:45:09 -0300

    codeclimate7

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,19 +6,16 @@ export default class SearchBar extends React.Component {
     return (
       <form>
         <label htmlFor="Text">Inclui o texto</label>
-        <input
-          type="text"
-          value={this.props.searchText}
-          onChange={this.props.onSearchTextChange}
+        <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}
         />
         <label htmlFor="Text">Mostrar somente favoritos</label>
-        <input
-          type="checkbox"
-          checked={this.props.bookmarkedOnly}
-          onChange={this.props.onBookmarkedChange}
+        <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}
         />
         <label htmlFor="Text">Filtrar por gênero</label>
-        <select name="Generos" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+        <select
+        name="Generos"
+        value={this.props.selectedGenre}
+        onChange={this.props.onSelectedGenreChange}>
           <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
