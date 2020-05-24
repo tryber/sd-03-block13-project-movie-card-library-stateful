5673a7ca7d6ae9db39fbf54c1b00a05b8547e9cd
leonardodamasceno8 <leonardodamasceno8@users>
2020-05-23 23:23:20 -0300

    requisito 1 - noCodeClimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+const genreArray = [
+  { text: 'Todos', value: '' },
+  { text: 'Ação', value: 'action' },
+  { text: 'Comédia', value: 'comedy' },
+  { text: 'Suspense', value: 'thriller' },
+];
+
 class SearchBar extends Component {
   render() {
     const { searchText, bookmarkedOnly, selectedGenre } = this.props;
@@ -7,14 +14,22 @@ class SearchBar extends Component {
     return (
       <div>
         <form>
-          <label htmlFor="text"> Incluir o texto
-            <input type="text" />
+          <label htmlFor="text"> Inclui o texto:
+            <input type="text" value={searchText} onChange={onSearchTextChange} />
           </label>
           <label htmlFor="check"> Mostrar somente favoritos
-            <input type="checkbox" />
+            <input type="checkbox" value={bookmarkedOnly} onChange={onBookmarkedChange} />
           </label>
-          <label htmlFor="genger"> Filtrar por genero
-            <select id="genger"></select>
+          <label htmlFor="genger"> Filtrar por gênero
+            <select id="genger" value={selectedGenre} onChange={onSelectedGenreChange} >
+              {
+                genreArray.map(({ text, value }) => (
+                  <option key={text} value={value}>
+                    {text}
+                  </option>
+                ))
+              }
+            </select>
           </label>
         </form>
       </div>
